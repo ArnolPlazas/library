89d298cb19a8424344bf11df07d5628f7cda3d83
Arnol Plazas <plazasarnol@gmail.com>
2023-03-31 10:13:29 -0500

    Incio del proyecto.

---
+++ library/settings/local.py
@@ -0,0 +1,28 @@
+from .base import  *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'librarydb',
+
+        'USER': 'arnolapp',
+
+        'PASSWORD': '1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
